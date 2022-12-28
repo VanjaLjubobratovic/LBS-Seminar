1ef49903fc4961cad1afd56459511fe25ade3ecf
VanjaLjubobratovic <vanja1708@gmail.com>
2022-12-28 12:59:57 +0100

    Krk zoom and multiple tiles merge

--- Seminar.R
+++ Seminar.R
@@ -1,14 +1,24 @@
 rm(list=ls())
 
-wd <- ("/home/vanja/Desktop/LBS-Seminar/")
+wd <- ("E:\\Fakultet\\Usluge zasnovane na lokaciji\\Seminar")
 setwd(wd)
 
 library(raster)
 library(rgdal)
 
-#elevation <- raster(paste0(wd, "N45E014.SRTMGL1.hgt.zip"))
-elevation <- raster(paste0(wd, "srtm_40_04.tif"))
-elevation
+elevationUp <- raster(paste0("N45E014.hgt"))
+elevationDown <- raster(paste0("N44E014.hgt"))
+elevationMerge <- mosaic(elevationUp, elevationDown, fun = mean) # S ovom funkcijom spajam ove odvojene karte
+#elevation <- raster(paste0(wd, "N44E014.hgt"))
+#elevation
 
-image(elevation)
-plot(elevation, main="Digital elevation model", maxpixels = 2000000)
+#image(elevationMerge)
+#plot(elevationMerge, main="Digital elevation model", maxpixels = 2000000)
+
+pt <- cbind(14.61, 45.1) # Koordinate centra Krka stavio odokativno
+points(pt) # S ovim ih prikazao
+
+size <- 0.21
+e <- extent(pt[1]-size, pt[1]+size, pt[2]-size, pt[2]+size)
+
+plot(elevationMerge, main = "Digital elevation model", ext = e)
