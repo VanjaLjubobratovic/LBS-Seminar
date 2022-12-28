1e7eeed44f0534c6006e0ab49e724fd882f8b331
VanjaLjubobratovic <vanja1708@gmail.com>
2022-12-28 13:11:18 +0100

    Attempt at Voronoi

--- Seminar.R
+++ Seminar.R
@@ -1,10 +1,11 @@
 rm(list=ls())
 
-wd <- ("E:\\Fakultet\\Usluge zasnovane na lokaciji\\Seminar")
+wd <- ("/home/student/R/Seminar")
 setwd(wd)
 
 library(raster)
 library(rgdal)
+library(ggvoronoi)
 
 elevationUp <- raster(paste0("N45E014.hgt"))
 elevationDown <- raster(paste0("N44E014.hgt"))
@@ -15,10 +16,22 @@ elevationMerge <- mosaic(elevationUp, elevationDown, fun = mean) # S ovom funkci
 #image(elevationMerge)
 #plot(elevationMerge, main="Digital elevation model", maxpixels = 2000000)
 
+
+
 pt <- cbind(14.61, 45.1) # Koordinate centra Krka stavio odokativno
-points(pt) # S ovim ih prikazao
+# S ovim ih prikazao
 
 size <- 0.21
 e <- extent(pt[1]-size, pt[1]+size, pt[2]-size, pt[2]+size)
 
 plot(elevationMerge, main = "Digital elevation model", ext = e)
+points(pt)
+
+
+ed = as.data.frame(elevationMerge, xy = TRUE)  #pretvara se u data frame, ima x,y (koordinate) i layer kao visinu
+
+ed = ed[seq(1, nrow(ed), 10000), ] #uzimam svaki 10000 podatak jer mi se sa svim podacima crasha R,
+#                                   a dovoljno je za neke osnovne testove
+
+
+ggplot(ed) + geom_voronoi(aes(x,y, fill = layer)) #voronoi, treba promjenit boju, dat vise podataka itd.
