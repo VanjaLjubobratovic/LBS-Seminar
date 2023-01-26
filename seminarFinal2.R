7f62ebc926e8f42150d17ddc3638460885569a42
VanjaLjubobratovic <vanja1708@gmail.com>
2023-01-26 14:43:11 +0100

    Added random point sampling and residual calculation

--- seminarFinal2.R
+++ seminarFinal2.R
@@ -9,9 +9,10 @@ library(sf)
 library(remotes)
 library(rgeoboundaries)
 library(data.table)
+library(rgeos)
 
-elevationUp <- raster(paste0("N45E014.hgt"))
-elevationDown <- raster(paste0("N44E014.hgt"))
+elevationUp <- raster(paste0("N45E014.hgt.zip"))
+elevationDown <- raster(paste0("N44E014.hgt.zip"))
 elevationMerge <- mosaic(elevationUp, elevationDown, fun = mean)
 
 print(elevationMerge)
@@ -35,8 +36,10 @@ ggplot(krk_geom) + geom_sf()
 
 srtmPoints <- rasterToPoints(map)
 srtmFrame <- data.frame(srtmPoints)
+proj4string(map)
 
 colnames(srtmFrame) = c("lat", "lon", "alt")
+srtmFull <- srtmFrame
 
 lonvals <- srtmFrame$lon
 latvals <- srtmFrame$lat
@@ -65,15 +68,40 @@ colnames(longSrtm) = c("lat", "lon", "value")
 #vrijednost geografske visine se pretvara u numeric za korištenje u Voronoi-u
 longSrtm$lon = as.numeric(as.character(longSrtm$lon))
 
-ggplot(longSrtm) +
-  scale_fill_gradientn("Elevation", 
-                       colors=c("seagreen","darkgreen","green1","yellow","gold4", "sienna"),
-                       values=scales::rescale(c(-5,0,50,100,200,500))) + 
-  scale_colour_gradientn("Elevation", 
-                        colors=c("seagreen","darkgreen","green1","yellow","gold4", "sienna"),
-                        values=scales::rescale(c(-5,0,50,100,200,500))) + 
-  theme_minimal() +
-  theme(axis.text=element_blank(),
-        axis.title=element_blank()) +
-  geom_path(data=krk_dt, aes(lon, lat, group=group), color="black", size=1.5) +
-  geom_voronoi(aes(lon, lat, fill = value), outline=krk_dt)
+krk_voronoi <- voronoi_polygon(data=longSrtm, x="lon", y="lat", outline=krk_dt)
+
+set.seed(123)
+n_points <- 500
+random_indeces <- sample(1:nrow(srtmFull), size=n_points, replace=FALSE)
+random_points_df <- srtmFull[random_indeces,]
+random_points <- SpatialPointsDataFrame(coords=cbind(random_points_df$lat, random_points_df$lon),
+                                        data=data.frame(value=random_points_df$alt))
+
+#Getting a list of all points which are contained within voronoi polygon
+points_over_voronoi <- over(random_points, krk_voronoi)
+
+#Removing values at indeces which contain NA
+indeces_not_na <- which(!is.na(points_over_voronoi$group))
+points_over_voronoi <- points_over_voronoi[indeces_not_na,]
+random_points <- random_points[indeces_not_na,]
+
+#Calculating difference between prediction and real value
+height_diff <- random_points$value - points_over_voronoi$value
+points_in_polygon <- gContains(krk_voronoi, random_points, byid = TRUE)
+
+plot(krk_voronoi)
+plot(random_points, col="red", pch=20, add=TRUE)
+
+# ggplot(longSrtm) +
+#   scale_fill_gradientn("Elevation", 
+#                        colors=c("seagreen","darkgreen","green1","yellow","gold4", "sienna"),
+#                        values=scales::rescale(c(-5,0,50,100,200,500))) + 
+#   scale_colour_gradientn("Elevation", 
+#                         colors=c("seagreen","darkgreen","green1","yellow","gold4", "sienna"),
+#                         values=scales::rescale(c(-5,0,50,100,200,500))) + 
+#   theme_minimal() +
+#   theme(axis.text=element_blank(),
+#         axis.title=element_blank()) +
+#   geom_path(data=krk_dt, aes(lon, lat, group=group), color="black", size=1.5) +
+#   geom_voronoi(aes(lon, lat, fill = value), outline=krk_dt)
+
